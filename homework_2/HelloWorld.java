1d2f7c2ec8c05c34fe9fbbcf624753adb0f06df9
ArnavPatel09 <52336272+ArnavPatel09@users.noreply.github.com>
2021-10-19 18:44:15 -0400

    Create HelloWorld.java
    Created a new folder called homework_2 with the walkthrough file HelloWorld.java

---
+++ homework_2/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * A program that prints Hello World! 
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2021 -- Homework 02 -- Walkthrough</p>
+ *
+ * @author Purdue CS 
+ * @version August 23, 2021
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+       System.out.println("Hello World!");
+    }
+}
